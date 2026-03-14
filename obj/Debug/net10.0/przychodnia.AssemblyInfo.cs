c5f743a8ceaa3f215536fd8bdf601559482eb5af
polskalok <polskalok85@gmail.com>
2026-03-14 19:49:23 +0100

    pierwsza wersja
    - Można zalogować sie do konta admina (admin:admin, dwa razy trzeba wpisać, jeszcze nie rozwiązany błąd)
    - działa przycisk Podgląd
    - dodano "na sztywno" (bez bazy danych) 3 przykładowych użytkowników
    - Dodano przyciski, działa pierwszy który prowadzi do panelu logowania

--- obj/Debug/net10.0/przychodnia.AssemblyInfo.cs
+++ obj/Debug/net10.0/przychodnia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("przychodnia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc07cc594b2dab4df20e3cc7f0f646eae96b195")]
 [assembly: System.Reflection.AssemblyProductAttribute("przychodnia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("przychodnia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
