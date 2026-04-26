a8bb6c496eecaf8e74015dfb5bdf2e78f2f9d870
Emenkej <149242320+Emenkej@users.noreply.github.com>
2026-04-26 11:51:22 +0200

    Dodanie testów do Modułu 2 i 3

---
+++ przychodnia.Tests/LogowanieTests.cs
@@ -0,0 +1,405 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using przychodnia.Controllers;
+using przychodnia.Data;
+using przychodnia.Models;
+using przychodnia.Services;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace przychodnia.Tests
+{
+    public class LogowanieTests
+    {
+        private ApplicationDbContext PrzygotujSztucznaBaze()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+
+
+        [Fact]
+        public void TC_01_Modul3_PoprawneLogowanie()
+        {
+            var db = PrzygotujSztucznaBaze();
+
+
+            var zaszyfrowaneHaslo = PasswordHasher.HashPassword("MojeTajneHaslo123!");
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "admin_test",
+                Haslo = zaszyfrowaneHaslo,
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "jan@test.pl",
+                Pesel = "11111111111",
+                Permisje = 1
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+
+            var result = controller.Login("admin_test", "MojeTajneHaslo123!") as RedirectToActionResult;
+
+
+            Assert.NotNull(result);
+            Assert.Equal("AdminPanel", result.ActionName);
+        }
+
+        [Fact]
+        public void TC_02_Modul3_BledneHaslo()
+        {
+            var db = PrzygotujSztucznaBaze();
+
+            var zaszyfrowaneHaslo = PasswordHasher.HashPassword("PrawdziweHaslo123!");
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "user_test",
+                Haslo = zaszyfrowaneHaslo,
+                CzyAktywny = true,
+                Imie = "Anna",
+                Nazwisko = "Nowak",
+                Email = "anna@test.pl",
+                Pesel = "22222222222",
+                Permisje = 4
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            var result = controller.Login("user_test", "ZupelnieZleHaslo999!") as ViewResult;
+
+
+            Assert.NotNull(result);
+            Assert.Equal("Błędne hasło lub login", controller.ViewBag.Error);
+        }
+
+        [Fact]
+        public void TC_03_Modul3_BlokadaKontaPo3Probach()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "lockout_user",
+                Haslo = PasswordHasher.HashPassword("MojeHaslo123!"),
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "j@test.pl",
+                Pesel = "11111111111"
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            controller.Login("lockout_user", "ZleHaslo1!");
+            controller.Login("lockout_user", "ZleHaslo2!");
+            controller.Login("lockout_user", "ZleHaslo3!");
+
+
+            var result = controller.Login("lockout_user", "ZleHaslo4!") as ViewResult;
+
+
+            Assert.NotNull(result);
+            Assert.Contains("Konto zostało zablokowane czasowo", controller.ViewBag.Error?.ToString() ?? "");
+        }
+
+        [Fact]
+        public void TC_04_Modul3_LogowanieNaZablokowaneKonto()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "blocked_user",
+                Haslo = PasswordHasher.HashPassword("PoprawneHaslo123!"),
+                CzyAktywny = true,
+                Imie = "Anna",
+                Nazwisko = "Nowak",
+                Email = "a@test.pl",
+                Pesel = "22222222222"
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            controller.Login("blocked_user", "ZleHaslo1!");
+            controller.Login("blocked_user", "ZleHaslo2!");
+            controller.Login("blocked_user", "ZleHaslo3!");
+
+
+            var result = controller.Login("blocked_user", "PoprawneHaslo123!");
+
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Contains("Konto zostało zablokowane czasowo", controller.ViewBag.Error?.ToString() ?? "");
+        }
+
+        [Fact]
+        public void TC_05_Modul3_WylogowanieZSystemu()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var controller = new AccountController(db);
+
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+
+
+            var result = controller.Logout() as RedirectToActionResult;
+
+
+            Assert.NotNull(result);
+
+            Assert.Equal("Login", result.ActionName);
+        }
+
+        [Fact]
+        public void TC_08_Modul3_WalidacjaKryteriowHasla()
+        {
+
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "nowy_testowy",
+                Haslo = "slabe",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "j@test.pl",
+                Pesel = "11111111111",
+                Telefon = "123456789"
+            };
+
+            var kontekstWalidacji = new System.ComponentModel.DataAnnotations.ValidationContext(uzytkownik);
+            var wyniki = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+
+            bool czyPoprawne = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(uzytkownik, kontekstWalidacji, wyniki, true);
+
+
+            Assert.False(czyPoprawne);
+            Assert.Contains(wyniki, w => w.MemberNames.Contains("Haslo"));
+        }
+
+        [Fact]
+        public void TC_09_Modul3_WalidacjaHasla_BrakZnakuSpecjalnego()
+        {
+
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "test_09",
+                Haslo = "WielkaMala1",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "j@test.pl",
+                Pesel = "11111111111"
+            };
+
+            var kontekstWalidacji = new System.ComponentModel.DataAnnotations.ValidationContext(uzytkownik);
+            var wyniki = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool czyPoprawne = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(uzytkownik, kontekstWalidacji, wyniki, true);
+
+
+            Assert.False(czyPoprawne);
+            Assert.Contains(wyniki, w => w.MemberNames.Contains("Haslo"));
+        }
+
+        [Fact]
+        public void TC_10_Modul3_WalidacjaHasla_BrakCyfry()
+        {
+
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "test_10",
+                Haslo = "WielkaMala!",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "j@test.pl",
+                Pesel = "11111111111"
+            };
+
+            var kontekstWalidacji = new System.ComponentModel.DataAnnotations.ValidationContext(uzytkownik);
+            var wyniki = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool czyPoprawne = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(uzytkownik, kontekstWalidacji, wyniki, true);
+
+
+            Assert.False(czyPoprawne);
+            Assert.Contains(wyniki, w => w.MemberNames.Contains("Haslo"));
+        }
+
+        [Fact]
+        public void TC_11_Modul3_WalidacjaHasla_BrakWielkichIMalychLiter()
+        {
+
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "test_11",
+                Haslo = "samemale1!",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "j@test.pl",
+                Pesel = "11111111111"
+            };
+
+            var kontekstWalidacji = new System.ComponentModel.DataAnnotations.ValidationContext(uzytkownik);
+            var wyniki = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool czyPoprawne = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(uzytkownik, kontekstWalidacji, wyniki, true);
+
+
+            Assert.False(czyPoprawne);
+            Assert.Contains(wyniki, w => w.MemberNames.Contains("Haslo"));
+        }
+
+        [Fact]
+        public void TC_12_Modul3_WalidacjaHasla_NieprawidlowaDlugosc()
+        {
+
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "test_12",
+                Haslo = "Aa1!x",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "j@test.pl",
+                Pesel = "11111111111"
+            };
+
+            var kontekstWalidacji = new System.ComponentModel.DataAnnotations.ValidationContext(uzytkownik);
+            var wyniki = new System.Collections.Generic.List<System.ComponentModel.DataAnnotations.ValidationResult>();
+            bool czyPoprawne = System.ComponentModel.DataAnnotations.Validator.TryValidateObject(uzytkownik, kontekstWalidacji, wyniki, true);
+
+
+            Assert.False(czyPoprawne);
+            Assert.Contains(wyniki, w => w.MemberNames.Contains("Haslo"));
+        }
+
+        [Fact]
+        public async Task TC_13_Modul3_OdzyskiwanieHasla_PoprawneDane()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "zapominalski",
+                Email = "ratunku@test.pl",
+                CzyAktywny = true,
+                Haslo = "StareHaslo123!"
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            await db.SaveChangesAsync();
+
+            var controller = new AccountController(db);
+
+            
+            var result = await controller.OdzyskajHaslo("zapominalski", "ratunku@test.pl") as RedirectToActionResult;
+
+            
+            var zaktualizowany = db.Uzytkownicy.First();
+            Assert.True(zaktualizowany.MuszZmieniHaslo); 
+            Assert.NotEqual("StareHaslo123!", zaktualizowany.Haslo); 
+        }
+
+        [Fact]
+        public async Task TC_14_Modul3_OdzyskiwanieHasla_BledneDane()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var controller = new AccountController(db);
+
+            
+            var result = await controller.OdzyskajHaslo("nieznany", "zlymail@test.pl") as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Podane dane nie pasują do żadnego konta w systemie.", controller.ViewBag.Error);
+        }
+
+        [Fact]
+        public void TC_16_Modul3_WymuszonaZmianaHaslaPoResecie()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "user_reset",
+                Haslo = PasswordHasher.HashPassword("Tymczasowe1!"),
+                CzyAktywny = true,
+                MuszZmieniHaslo = true 
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+            controller.ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext() };
+
+            
+            var result = controller.Login("user_reset", "Tymczasowe1!") as RedirectToActionResult;
+
+            
+            Assert.NotNull(result);
+            Assert.Equal("MuszZmieniHaslo", result.ActionName);
+        }
+
+        [Fact]
+        public void TC_17_Modul3_WeryfikacjaZgodnosciHasel()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik { Login = "test_17", CzyAktywny = true };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+            
+            var result = controller.ZmieniHaslo(uzytkownik.ID, "NoweHaslo123!", "CalkowicieInneHaslo123!") as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Upewnij się że hasła są identyczne", controller.ViewBag.Error);
+        }
+
+        [Fact]
+        public void TC_18_Modul3_WalidacjaHistoriiHasel()
+        {
+            var db = PrzygotujSztucznaBaze();
+
+            
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "historia_test",
+                CzyAktywny = true,
+                OstatniaHasla = PasswordHasher.HashPassword("StareHaslo1!")
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+            
+            var result = controller.ZmieniHaslo(uzytkownik.ID, "StareHaslo1!", "StareHaslo1!") as ViewResult;
+
+            
+            Assert.NotNull(result);
+            Assert.Equal("Nowe hasło musi różnić się od 3 ostatnich używanych haseł.", controller.ViewBag.Error);
+        }
+    }
+}
---
+++ przychodnia.Tests/UprawnieniaTests.cs
@@ -0,0 +1,211 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using przychodnia.Controllers;
+using przychodnia.Data;
+using przychodnia.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace przychodnia.Tests
+{
+    public class UprawnieniaTests
+    {
+        private ApplicationDbContext PrzygotujSztucznaBaze()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new ApplicationDbContext(options);
+        }
+
+
+        [Fact]
+        public void TC_1_Modul2_PrzegladListyWszystkichUprawnien_WyswietlaKompletnaListe()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var controller = new AccountController(db);
+
+
+            var result = controller.ListaUprawnien() as ViewResult;
+
+
+            var model = result?.Model as IEnumerable<dynamic>;
+
+            Assert.NotNull(result);
+            Assert.NotNull(model);
+            Assert.Equal(3, model.Count());
+        }
+
+        [Fact]
+        public void TC_2_Modul2_NadanieUprawnienUzytkownikowi_ZapisujeZmianyWBazie()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "jan.kowalski",
+                Haslo = "Test1!",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "jan@test.pl",
+                Pesel = "12345678901",
+                Permisje = 0
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            int[] przypisaneRole = new int[] { 1, 2 };
+
+
+            var result = controller.Uprawnienia(uzytkownik.ID, przypisaneRole) as RedirectToActionResult;
+
+
+            var zaktualizowany = db.Uzytkownicy.First();
+            Assert.Equal(3, zaktualizowany.Permisje);
+
+
+            Assert.NotNull(result);
+            Assert.Equal("Podglad", result.ActionName);
+        }
+
+        [Fact]
+        public void TC_3_Modul2_NadanieUprawnien_BrakWyboru()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "jan.kowalski",
+                Haslo = "Test1!",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "jan@test.pl",
+                Pesel = "12345678901",
+                Permisje = 4
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            int[] pusteRole = new int[0];
+            var result = controller.Uprawnienia(uzytkownik.ID, pusteRole) as ViewResult;
+
+
+            Assert.NotNull(result);
+            Assert.Equal("nie zaznaczono żadnych uprawnień", controller.ViewBag.Error);
+        }
+
+        [Fact]
+        public void TC_4_Modul2_PrzegladUzytkownikowPDanymUprawnieniem()
+        {
+            var db = PrzygotujSztucznaBaze();
+
+
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "admin1", CzyAktywny = true, Imie = "A", Nazwisko = "B", Email = "a@test.pl", Pesel = "11111111111", Permisje = 1 }); // Admin
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "pracownik1", CzyAktywny = true, Imie = "C", Nazwisko = "D", Email = "b@test.pl", Pesel = "22222222222", Permisje = 2 }); // Pracownik
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "super_admin", CzyAktywny = true, Imie = "E", Nazwisko = "F", Email = "c@test.pl", Pesel = "33333333333", Permisje = 3 }); // Admin (1) + Pracownik (2) = 3
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "nieaktywny_admin", CzyAktywny = false, Imie = "G", Nazwisko = "H", Email = "d@test.pl", Pesel = "44444444444", Permisje = 1 }); // Usunięty admin
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            var result = controller.UzytkownicyZUprawnieniem(1) as ViewResult;
+            var model = result?.Model as List<Uzytkownik>;
+
+
+            Assert.NotNull(model);
+            Assert.Equal(2, model.Count);
+            Assert.Contains(model, u => u.Login == "admin1");
+            Assert.Contains(model, u => u.Login == "super_admin");
+            Assert.DoesNotContain(model, u => u.Login == "pracownik1");
+            Assert.DoesNotContain(model, u => u.Login == "nieaktywny_admin");
+        }
+
+        [Fact]
+        public void TC_5_Modul2_PrzegladUzytkownikowZUprawnieniem_BrakWynikow()
+        {
+            var db = PrzygotujSztucznaBaze();
+
+
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "admin_only", CzyAktywny = true, Imie = "A", Nazwisko = "B", Email = "a@test.pl", Pesel = "11111111111", Permisje = 1 });
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            var result = controller.UzytkownicyZUprawnieniem(2) as ViewResult;
+            var model = result?.Model as List<Uzytkownik>;
+
+
+            Assert.NotNull(model);
+            Assert.Empty(model);
+        }
+
+        [Fact]
+        public void TC_6_Modul2_OdebranieUprawnienUzytkownikowi()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "wielo_rolowy",
+                Haslo = "Test1!",
+                CzyAktywny = true,
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "jan@test.pl",
+                Pesel = "12345678901",
+                Permisje = 3
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            int[] noweRole = new int[] { 1 };
+            var result = controller.Uprawnienia(uzytkownik.ID, noweRole) as RedirectToActionResult;
+
+
+            var zaktualizowany = db.Uzytkownicy.First();
+            Assert.Equal(1, zaktualizowany.Permisje);
+        }
+
+        [Fact]
+        public void TC_7_Modul2_NadanieUprawnienZapomnianemuUzytkownikowi()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var zapomniany = new Uzytkownik
+            {
+                Login = "zamazany_login",
+                CzyAktywny = false,
+                Imie = "Anonim",
+                Nazwisko = "Uzytkownik",
+                Email = "anonim@test.pl",
+                Pesel = "00000000000",
+                Permisje = 0
+            };
+            db.Uzytkownicy.Add(zapomniany);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+
+            int[] noweRole = new int[] { 1 };
+            controller.Uprawnienia(zapomniany.ID, noweRole);
+
+
+            var stanPoAkcji = db.Uzytkownicy.First();
+
+
+            Assert.Equal(0, stanPoAkcji.Permisje);
+        }
+    }
+}
