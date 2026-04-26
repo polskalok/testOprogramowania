9419e8ff749327fe6b5ab5fb6a1a0a2ddba9269b
DawidRozniatowski <dawid.rozniatowski@onet.pl>
2026-04-26 02:00:22 +0200

    v5
    Dodano:
    -Reset zapomnianego hasła
    -Zmianę hasła użytkownika przez admina
    -Blokadę konta na czas 2 godzin przy trzech nieudanych próbach wpisania hasła

---
+++ Migrations/20260425005638_AddLockoutToUzytkownik.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace przychodnia.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLockoutToUzytkownik : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Migration modifies existing table: add columns for lockout support
+            migrationBuilder.AddColumn<int>(
+                name: "FailedLoginAttempts",
+                table: "Uzytkownicy",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockoutEnd",
+                table: "Uzytkownicy",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedLoginAttempts",
+                table: "Uzytkownicy");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnd",
+                table: "Uzytkownicy");
+        }
+    }
+}
---
+++ Migrations/20260425231306_AddPasswordChangeFlags.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace przychodnia.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordChangeFlags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MuszZmieniHaslo",
+                table: "Uzytkownicy",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OstatniaHasla",
+                table: "Uzytkownicy",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MuszZmieniHaslo",
+                table: "Uzytkownicy");
+
+            migrationBuilder.DropColumn(
+                name: "OstatniaHasla",
+                table: "Uzytkownicy");
+        }
+    }
+}
