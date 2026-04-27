c83f3b6ff3ec5bda035198288e4e3443470958cd
Emenkej <149242320+Emenkej@users.noreply.github.com>
2026-04-27 15:18:05 +0200

    Add files via upload

--- przychodnia.Tests/KontrolerUzytkownikaTests.cs
+++ przychodnia.Tests/KontrolerUzytkownikaTests.cs
@@ -11,7 +11,7 @@ namespace przychodnia.Tests.Scenariusze
 {
     public class KontrolerUzytkownikaTests
     {
-        
+
         private ApplicationDbContext PrzygotujSztucznaBaze()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -22,129 +22,176 @@ namespace przychodnia.Tests.Scenariusze
         }
 
         [Fact]
-        public void TC_3_DodanieUzytkownika_DuplikatLoginu_ZwracaBlad()
+        public void TC_3_DodanieUzytkownika_DuplikatLoginu()
         {
-            
             var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "admin123", Haslo = "Haslo1!", Email = "stary@test.pl", Pesel = "11111111111" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "admin123", Haslo = "MocneHaslo123!", Email = "stary@test.pl", Pesel = "11111111111" });
             db.SaveChanges();
 
             var controller = new AccountController(db);
-
             
-            var nowyUzytkownik = new Uzytkownik { Login = "admin123", Haslo = "Haslo2!", Email = "nowy@test.pl", Pesel = "22222222222", Plec = "M" };
+            var nowyUzytkownik = new Uzytkownik { Login = "admin123", Haslo = "MocneHaslo123!", Email = "nowy@test.pl", Pesel = "22222222222", Plec = "M" };
 
-            
             controller.DodajUzytkownik(nowyUzytkownik);
 
-            
             Assert.False(controller.ModelState.IsValid);
             Assert.True(controller.ModelState.ContainsKey("Login"));
             Assert.Equal("Login już istnieje", controller.ModelState["Login"].Errors[0].ErrorMessage);
         }
 
         [Fact]
-        public void TC_4_DodanieUzytkownika_DuplikatPesel_ZwracaBlad()
+        public void TC_4_DodanieUzytkownika_DuplikatPESEL()
         {
-           
             var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "staryLogin", Haslo = "Haslo1!", Email = "stary@test.pl", Pesel = "85010112345" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "staryLogin", Haslo = "MocneHaslo123!", Email = "stary@test.pl", Pesel = "85010112345" });
             db.SaveChanges();
 
             var controller = new AccountController(db);
+            var nowyUzytkownik = new Uzytkownik { Login = "nowyLogin", Haslo = "MocneHaslo123!", Email = "nowy@test.pl", Pesel = "85010112345", Plec = "M" };
 
-            
-            var nowyUzytkownik = new Uzytkownik { Login = "nowyLogin", Haslo = "Haslo2!", Email = "nowy@test.pl", Pesel = "85010112345", Plec = "M" };
-
-            
             controller.DodajUzytkownik(nowyUzytkownik);
 
-            
             Assert.False(controller.ModelState.IsValid);
             Assert.True(controller.ModelState.ContainsKey("Pesel"));
             Assert.Equal("PESEL już istnieje w systemie", controller.ModelState["Pesel"].Errors[0].ErrorMessage);
         }
 
         [Fact]
-        public void TC_5_DodanieUzytkownika_DuplikatEmail_ZwracaBlad()
+        public void TC_5_DodanieUzytkownika_DuplikatEmail()
         {
-            
             var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "staryLogin", Haslo = "Haslo1!", Email = "jan.kowalski@test.pl", Pesel = "11111111111" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "staryLogin", Haslo = "MocneHaslo123!", Email = "jan.kowalski@test.pl", Pesel = "11111111111" });
             db.SaveChanges();
 
             var controller = new AccountController(db);
+            var nowyUzytkownik = new Uzytkownik { Login = "nowyLogin", Haslo = "MocneHaslo123!", Email = "jan.kowalski@test.pl", Pesel = "22222222222", Plec = "M" };
 
-            
-            var nowyUzytkownik = new Uzytkownik { Login = "nowyLogin", Haslo = "Haslo2!", Email = "jan.kowalski@test.pl", Pesel = "22222222222", Plec = "M" };
-
-            
             controller.DodajUzytkownik(nowyUzytkownik);
 
-            
             Assert.False(controller.ModelState.IsValid);
             Assert.True(controller.ModelState.ContainsKey("Email"));
             Assert.Equal("E-mail już istnieje w systemie", controller.ModelState["Email"].Errors[0].ErrorMessage);
         }
 
         [Fact]
-        public void TC_6_DodanieUzytkownika_NiepoprawnyPeselData_ZwracaBlad()
+        public void TC_6_DodanieUzytkownika_NiepoprawnyPeselData()
         {
-            
             var db = PrzygotujSztucznaBaze();
             var controller = new AccountController(db);
-            
-            var nowyUzytkownik = new Uzytkownik { Login = "test1", Haslo = "Haslo1!", Email = "test@test.pl", Pesel = "85013212345", Plec = "M" };
+            var nowyUzytkownik = new Uzytkownik { Login = "test1", Haslo = "MocneHaslo123!", Email = "test@test.pl", Pesel = "85013212345", Plec = "M" };
 
-            
             controller.DodajUzytkownik(nowyUzytkownik);
 
-            
             Assert.False(controller.ModelState.IsValid);
             Assert.True(controller.ModelState.ContainsKey("Pesel"));
             Assert.Equal("PESEL nieprawidłowy – niepoprawna data", controller.ModelState["Pesel"].Errors[0].ErrorMessage);
         }
 
         [Fact]
-        public void TC_7_DodanieUzytkownika_NiepoprawnyPeselPlec_ZwracaBlad()
+        public void TC_7_DodanieUzytkownika_NiepoprawnyPeselPlec()
         {
-            
             var db = PrzygotujSztucznaBaze();
             var controller = new AccountController(db);
-            
-            var nowyUzytkownik = new Uzytkownik { Login = "test2", Haslo = "Haslo1!", Email = "test@test.pl", Pesel = "85010112346", Plec = "M" };
+            var nowyUzytkownik = new Uzytkownik { Login = "test2", Haslo = "MocneHaslo123!", Email = "test@test.pl", Pesel = "85010112346", Plec = "M" };
 
-            
             controller.DodajUzytkownik(nowyUzytkownik);
 
-           
             Assert.False(controller.ModelState.IsValid);
             Assert.True(controller.ModelState.ContainsKey("Pesel"));
+            // Używamy długiej kreski, tak jak wpisali to devowie w kodzie
             Assert.Equal("PESEL nieprawidłowy – niepoprawna płeć", controller.ModelState["Pesel"].Errors[0].ErrorMessage);
         }
 
         [Fact]
-        public void TC_8_DodanieUzytkownika_NiepoprawnyPeselCyfraKontrolna_ZwracaBlad()
+        public void TC_8_DodanieUzytkownika_NiepoprawnyPeselCyfraKontrolna()
         {
-            
             var db = PrzygotujSztucznaBaze();
             var controller = new AccountController(db);
-            
-            var nowyUzytkownik = new Uzytkownik { Login = "test3", Haslo = "Haslo1!", Email = "test@test.pl", Pesel = "85010112344", Plec = "K" };
+            var nowyUzytkownik = new Uzytkownik { Login = "test3", Haslo = "MocneHaslo123!", Email = "test@test.pl", Pesel = "85010112344", Plec = "K" };
 
-            
             controller.DodajUzytkownik(nowyUzytkownik);
 
-            
             Assert.False(controller.ModelState.IsValid);
             Assert.True(controller.ModelState.ContainsKey("Pesel"));
-            Assert.Equal("PESEL nieprawidłowy – niepoprawna cyfra kontrolna", controller.ModelState["Pesel"]!.Errors[0].ErrorMessage);
+            // UWAGA: Ten test BĘDZIE CZERWONY, dopóki chłopaki nie naprawią u siebie tego "Błąd! Wyliczona...", o czym już im pisałeś!
+            Assert.Equal("PESEL nieprawidłowy - niepoprawna cyfra kontrolna", controller.ModelState["Pesel"].Errors[0].ErrorMessage);
         }
 
         [Fact]
-        public void TC_14_ZapomnienieUzytkownika_AnonimizujeDaneZgodnieZRODO()
+        public void TC_15_EdycjaUzytkownika_PoprawnyScenariusz()
         {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "jan.kowalski",
+                Haslo = "MocneHaslo123!",
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "jan.kowalski@test.pl",
+                Pesel = "85010112345"
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
             
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext()
+            };
+
+            uzytkownik.Imie = "Janusz";
+            uzytkownik.Email = "janusz.kowalski@test.pl";
+
+            controller.Edytuj(uzytkownik);
+
+            var zaktualizowany = db.Uzytkownicy.First();
+            Assert.Equal("Janusz", zaktualizowany.Imie);
+            Assert.Equal("janusz.kowalski@test.pl", zaktualizowany.Email);
+        }
+
+        [Fact]
+        public void TC_16_EdycjaUzytkownika_NaruszenieWalidacji()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var uzytkownik = new Uzytkownik
+            {
+                Login = "jan.kowalski",
+                Imie = "Jan",
+                Nazwisko = "Kowalski",
+                Email = "jan.kowalski@test.pl",
+                Pesel = "85010112345"
+            };
+            db.Uzytkownicy.Add(uzytkownik);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+            var zepsuteDane = new Uzytkownik
+            {
+                ID = uzytkownik.ID,
+                Login = "jan.kowalski",
+                Imie = "Janusz",
+                Nazwisko = "Kowalski",
+                Email = "niepoprawny@email",
+                Pesel = "85010112345"
+            };
+
+            controller.ModelState.AddModelError("Email", "Walidacja danych niepoprawna");
+
+            var result = controller.Edytuj(zepsuteDane);
+
+            Assert.False(controller.ModelState.IsValid);
+
+            var sprawdzWBazie = db.Uzytkownicy.First();
+            Assert.Equal("jan.kowalski@test.pl", sprawdzWBazie.Email);
+            Assert.Equal("Jan", sprawdzWBazie.Imie);
+        }
+
+        [Fact]
+        public void TC_17_ZapomnienieUzytkownika_PoprawnyScenariusz()
+        {
             var db = PrzygotujSztucznaBaze();
             var uzytkownik = new Uzytkownik
             {
@@ -163,32 +210,34 @@ namespace przychodnia.Tests.Scenariusze
             var controller = new AccountController(db);
 
             
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext()
+            };
+
             controller.Zapomnij(uzytkownik.ID);
 
-            
             var zapomniany = db.Uzytkownicy.First();
-            Assert.False(zapomniany.CzyAktywny); 
-            Assert.Equal(0, zapomniany.Permisje); 
+            Assert.False(zapomniany.CzyAktywny);
+            Assert.Equal(0, zapomniany.Permisje);
             Assert.NotEqual("jan.kowalski", zapomniany.Login);
-            Assert.NotEqual("Jan", zapomniany.Imie); 
-            Assert.NotEqual("jan@test.pl", zapomniany.Email); 
+            Assert.NotEqual("Jan", zapomniany.Imie);
+            Assert.NotEqual("jan@test.pl", zapomniany.Email);
         }
+
         [Fact]
-        public void TC_16_WyswietlanieListy_PokazujeTylkoAktywnych()
+        public void TC_20_WyswietlanieListy_TylkoAktywniUzytkownicy()
         {
-            
             var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "aktywny123", CzyAktywny = true, Nazwisko = "Kowalski", Email = "a@test.pl", Pesel = "11111111111" });
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "zapomniany123", CzyAktywny = false, Nazwisko = "Zamazany", Email = "z@test.pl", Pesel = "22222222222" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "aktywny123", CzyAktywny = true, Imie = "Jan", Nazwisko = "Kowalski", Email = "a@test.pl", Pesel = "11111111111" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "zapomniany123", CzyAktywny = false, Imie = "Zamazany", Nazwisko = "Zamazany", Email = "z@test.pl", Pesel = "22222222222" });
             db.SaveChanges();
 
             var controller = new AccountController(db);
 
-            
             var result = controller.AdminPanel(searchString: null, showForgotten: false) as ViewResult;
             var model = result?.Model as List<Uzytkownik>;
 
-            
             Assert.NotNull(model);
             Assert.Single(model);
             Assert.True(model[0].CzyAktywny);
@@ -196,77 +245,118 @@ namespace przychodnia.Tests.Scenariusze
         }
 
         [Fact]
-        public void TC_17_Wyszukiwanie_PoLoginie()
+        public void TC_21_WyswietlanieListy_BrakUzytkownikow()
         {
-           
             var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "jan.kowalski", CzyAktywny = true, Nazwisko = "Kowalski", Email = "j@test.pl", Pesel = "11111111111" });
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "inny.login", CzyAktywny = true, Nazwisko = "Nowak", Email = "i@test.pl", Pesel = "22222222222" });
-            db.SaveChanges();
-
             var controller = new AccountController(db);
 
-            
-            var result = controller.AdminPanel(searchString: "jan.kowalski", showForgotten: false) as ViewResult;
+            var result = controller.AdminPanel(searchString: null, showForgotten: false) as ViewResult;
             var model = result?.Model as List<Uzytkownik>;
 
-            
-            Assert.NotNull(model);
-            Assert.Single(model); 
-            Assert.Equal("jan.kowalski", model[0].Login);
-        }
-        [Fact]
-        public void TC_18_Wyszukiwanie_BrakWynikow_ZwracaPustaListe()
-        {
-            
-            var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "jan", CzyAktywny = true, Nazwisko = "Kowalski", Pesel = "11111111111" });
-            db.SaveChanges();
-            var controller = new AccountController(db);
-
-            
-            var result = controller.AdminPanel(searchString: "nieistniejacyuserxyz123", showForgotten: false) as ViewResult;
-            var model = result?.Model as List<Uzytkownik>;
-
-            
             Assert.NotNull(model);
             Assert.Empty(model);
         }
 
         [Fact]
-        public void TC_19_Wyswietlanie_ListaZapomnianych_PokazujeTylkoZanonimizowanych()
+        public void TC_22_WyszukiwanieUzytkownikow_PoLoginie()
         {
-            
             var db = PrzygotujSztucznaBaze();
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "aktywny", CzyAktywny = true, Nazwisko = "Kowalski", Pesel = "11111111111" });
-            db.Uzytkownicy.Add(new Uzytkownik { Login = "zapomniany", CzyAktywny = false, Nazwisko = "Zamazany", Pesel = "22222222222" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "jan.kowalski", CzyAktywny = true, Imie = "Jan", Nazwisko = "Kowalski", Email = "j@test.pl", Pesel = "11111111111" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "inny.login", CzyAktywny = true, Imie = "Inny", Nazwisko = "Nowak", Email = "i@test.pl", Pesel = "22222222222" });
             db.SaveChanges();
+
             var controller = new AccountController(db);
 
-            
+            var result = controller.AdminPanel(searchString: "jan.kowalski", showForgotten: false) as ViewResult;
+            var model = result?.Model as List<Uzytkownik>;
+
+            Assert.NotNull(model);
+            Assert.Single(model);
+            Assert.Equal("jan.kowalski", model[0].Login);
+        }
+
+        [Fact]
+        public void TC_23_WyszukiwanieUzytkownikow_BrakWynikow()
+        {
+            var db = PrzygotujSztucznaBaze();
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "jan.kowalski", CzyAktywny = true, Imie = "Jan", Nazwisko = "Kowalski", Email = "j@test.pl", Pesel = "11111111111" });
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+            var result = controller.AdminPanel(searchString: "nieistniejacyuserxyz123", showForgotten: false) as ViewResult;
+            var model = result?.Model as List<Uzytkownik>;
+
+            Assert.NotNull(model);
+            Assert.Empty(model);
+        }
+
+        [Fact]
+        public void TC_24_WyswietlanieListy_UzytkownicyZapomniani()
+        {
+            var db = PrzygotujSztucznaBaze();
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "aktywny", CzyAktywny = true, Imie = "Jan", Nazwisko = "Kowalski", Email = "a@test.pl", Pesel = "11111111111" });
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "zapomniany", CzyAktywny = false, Imie = "Zamazane", Nazwisko = "Zamazane", Email = "z@test.pl", Pesel = "22222222222" });
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
             var result = controller.AdminPanel(searchString: null, showForgotten: true) as ViewResult;
             var model = result?.Model as List<Uzytkownik>;
 
-            
             Assert.NotNull(model);
             Assert.Single(model);
             Assert.False(model[0].CzyAktywny);
         }
 
         [Fact]
-        public void TC_20_PodgladDanych_NiedostepnyDlaUzytkownikowZapomnianych()
+        public void TC_25_WyszukiwanieUzytkownikowZapomnianych_BrakWynikow()
         {
-            
             var db = PrzygotujSztucznaBaze();
-            var zapomniany = new Uzytkownik { Login = "zamazany", CzyAktywny = false, Nazwisko = "Anonim", Pesel = "00000000000" };
-            db.Uzytkownicy.Add(zapomniany);
+            db.Uzytkownicy.Add(new Uzytkownik { Login = "zapomniany", CzyAktywny = false, Imie = "Zamazane", Nazwisko = "Zamazane", Email = "z@test.pl", Pesel = "22222222222" });
             db.SaveChanges();
+
             var controller = new AccountController(db);
 
-            
+            var result = controller.AdminPanel(searchString: "nieistniejacy", showForgotten: true) as ViewResult;
+            var model = result?.Model as List<Uzytkownik>;
+
+            Assert.NotNull(model);
+            Assert.Empty(model);
+        }
+
+        [Fact]
+        public void TC_26_PodgladDanych_WyswietlaPelneDaneAktywnegoUzytkownika()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var aktywny = new Uzytkownik { Login = "jan.kowalski", CzyAktywny = true, Imie = "Jan", Nazwisko = "Kowalski", Email = "jan@test.pl", Pesel = "12345678901" };
+            db.Uzytkownicy.Add(aktywny);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
+            var result = controller.Podglad(aktywny.ID) as ViewResult;
+            var model = result?.Model as Uzytkownik;
+
+            Assert.NotNull(result);
+            Assert.NotNull(model);
+            Assert.Equal("jan.kowalski", model.Login);
+            Assert.Equal("Jan", model.Imie);
+            Assert.Equal("Kowalski", model.Nazwisko);
+        }
+
+        [Fact]
+        public void TC_26b_PodgladDanych_NiedostepnyDlaUzytkownikowZapomnianych()
+        {
+            var db = PrzygotujSztucznaBaze();
+            var zapomniany = new Uzytkownik { Login = "zamazany", CzyAktywny = false, Imie = "Zamazany", Nazwisko = "Zamazany", Email = "z@test.pl", Pesel = "00000000000" };
+            db.Uzytkownicy.Add(zapomniany);
+            db.SaveChanges();
+
+            var controller = new AccountController(db);
+
             var result = controller.Podglad(zapomniany.ID);
 
-            
             Assert.IsNotType<ViewResult>(result);
         }
     }
